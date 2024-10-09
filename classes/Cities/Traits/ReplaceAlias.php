1bc9790e7deb827e3a62ddc7335d9f90148ea98c
i.samarin <mako.mmw@gmail.com>
2024-10-09 16:07:17 +0300

    russian encoding fix

--- classes/Cities/Traits/ReplaceAlias.php
+++ classes/Cities/Traits/ReplaceAlias.php
@@ -24,23 +24,27 @@ trait ReplaceAlias
 
 
         if ($currentCity) {
-            $data = str_replace("[[?????]]", $currentCity, $data);
+            $data = str_replace("[[имгео]]", $currentCity, $data);
         }
         if ($currentCityPR) {
-            $data = str_replace("[[???????]]", $currentCityPR, $data);
+            $data = str_replace("[[местгео]]", $currentCityPR, $data);
         }
         if ($currentCityRP) {
-            $data = str_replace("[[??????]]", $currentCityRP, $data);
+            $data = str_replace("[[родгео]]", $currentCityRP, $data);
         }
         if ($currentEmail) {
             $data = str_replace("[[email]]", $currentEmail, $data);
         }
         if ($currentPhone) {
-            $data = str_replace("[[???????]]", $currentPhone, $data);
+            $data = str_replace("[[телефон]]", $currentPhone, $data);
         }
         if ($currentAddres) {
-            $data = str_replace("[[?????]]", $currentAddres, $data);
+            $data = str_replace("[[адрес]]", $currentAddres, $data);
             $data = str_replace("[[city]]", $currentAddres, $data);
+        }
+        if ($currentCityAlias) {
+            $data = str_replace("[[алиас]]", $currentCityAlias, $data);
+            $data = str_replace('href="/', 'href="/' . $currentCityAlias . '/', $data);
         }
         return $data;
     }
